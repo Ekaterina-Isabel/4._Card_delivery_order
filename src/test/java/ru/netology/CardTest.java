52b490b82e2dae385b6c1ad4177f078cb6034563
kate <vuk_kat@mail.ru>
2022-05-21 14:55:28 +0500

    add CardDeliveryOrderTest

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,4 +1,34 @@
 package ru.netology;
 
-public class CardTest {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+class CardDeliveryOrderTest {
+
+  @BeforeEach
+  void setup() {
+    open("http://localhost:9999");      //перейти на страницу по URL
+  }
+
+    @Test
+      void shouldSuccessfullyBookTheDeliveryOfTheCard() {
+        Configuration.holdBrowserOpen=true;     //после выполнения теста браузер останется открытым
+        $("[data-test-id=city] input").setValue("Екатеринбург");      //заполнить поле Город
+        $("[data-test-id=date] input").setValue("26.05.2022");      //заполнить поле Дата встречи
+        $("[data-test-id=name] input").setValue("Иванов-Смирнов Иван");       //заполнить поле Фамилия и имя
+        $("[data-test-id=phone] input").setValue("+71593577894");       //заполнить поле Мобильный телефон
+        $("[data-test-id=agreement]").click();     //нажать на чекбокс согласия
+        $x("//*[text()=\"Забронировать\"]").click();      //нажать на кнопку Забронировать
+
+      $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));    //проверка на наличие текста на странице, время ожидания 15 с
+    }
 }
