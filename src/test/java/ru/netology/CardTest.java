baf403accefa52806d0c31f591fae3fbc2916973
kate <vuk_kat@mail.ru>
2022-05-24 17:35:31 +0500

    fix comments

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,13 +1,13 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
@@ -16,26 +16,27 @@ import static com.codeborne.selenide.Selenide.*;
 
 class CardDeliveryOrderTest {
 
-  @BeforeEach
-  void setup() {
-    open("http://localhost:9999");      //перейти на страницу по URL
-  }
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
 
-  @Test
-      void shouldSuccessfullyBookTheDeliveryOfTheCard() {
-      Calendar c = new GregorianCalendar();
-      c.add(Calendar.DAY_OF_YEAR, 4);   // увеличиваем на 4 дней от текущей даты
-    SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");   //придаем нужный формат дате
-    String date = format.format(c.getTime());
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
-    $("[data-test-id=city] input").setValue("Екатеринбург");      //заполнить поле Город
+    @Test
+    void shouldSuccessfullyBookTheDeliveryOfTheCard() {
+        String date = generateDate(4);      //увеличиваем текущую дату на 4 дня
+        $("[data-test-id=city] input").setValue("Екатеринбург");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);      //заполнить поле Дата встречи
         $("[data-test-id=date] input").setValue(date);      //заполнить поле Дата встречи
-        $("[data-test-id=name] input").setValue("Иванов-Смирнов Иван");       //заполнить поле Фамилия и имя
-        $("[data-test-id=phone] input").setValue("+71593577894");       //заполнить поле Мобильный телефон
+        $("[data-test-id=name] input").setValue("Иванов-Смирнов Иван");
+        $("[data-test-id=phone] input").setValue("+71593577894");
         $("[data-test-id=agreement]").click();     //нажать на чекбокс согласия
-        $x("//*[text()=\"Забронировать\"]").click();      //нажать на кнопку Забронировать
+        $x("//*[text()=\"Забронировать\"]").click();
 
-      $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));    //проверка на наличие текста на странице, время ожидания 15 с
+        $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));    //проверка на наличие текста на странице, время ожидания 15 с
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15));
     }
 }
